3cf6553a5242132e3b46eeae0139a2df916a9f14
Divya Prabhu <85998740+422divya@users.noreply.github.com>
2023-08-04 18:08:50 +0530

    Update launch.tf

--- terraform-LB/launch.tf
+++ terraform-LB/launch.tf
@@ -1,7 +1,13 @@
-resource "aws_launch_template" "template" {
-    name = "launch-template"
+resource "aws_launch_template" "template" { 
+    name_prefix = "Dev-instance-"
     image_id = var.ec2-instance["ami"] 
     instance_type = var.ec2-instance["type"]
-    user_data = filebase64("script.sh") 
+    key_name = module.key_pair.key_pair_name
+
+   tags = {
+      Name = "dev-launch-template"
+    
 
 }
+user_data = filebase64("pubkey.sh")
+}  
